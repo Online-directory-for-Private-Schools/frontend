b73c0452d2ac1e5488cf0c249d47e78c09620bc6
Ahmed Djalal Hacini <ahmed.hacini@ensia.edu.dz>
2023-04-04 01:07:46 +0100

    Redirections not implemented

---
+++ src/Sign_up.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './Sign_up.css';
+import Login from './Login'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Sign_up() {
+
+    const [email, setEmail] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [passwd, setPasswd] = useState("");
+    const [category, setCategory] = useState("")
+    
+
+  return (
+    <div className="Sign_up">
+     
+    
+    <nav id="navbar" className="navbar">
+      <h2 className='logo'> Website Logo </h2>
+      <ul>
+          <li>Have an account? <a href=""> <span> Sign in </span></a> instead</li>
+          
+      </ul> 
+    </nav>
+    <div className="wrapper">
+	    <div className="registration_form">
+		  <div className="title">
+			Welcome to XXXX
+		  </div>
+
+		<form>
+			<div className="form_wrap">
+				<div className="input_grp">
+					<div className="input_wrap">
+						<label for="fname">First Name</label>
+						<input type="text" id="fname" value={firstname} onChange={(e) => {setFirstname(e.target.value)}} />
+					</div>
+					<div className="input_wrap">
+						<label for="lname">Last Name</label>
+						<input type="text" id="lname" value={lastname} onChange={(e) => {setLastName(e.target.value)}}/>
+					</div>
+				</div>
+				<div className="input_wrap">
+					<label for="email">Email</label>
+					<input type="email" id="email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+				</div>
+        <div className="input_wrap">
+					<label for="passwd">Password</label>
+					<input type="password" id="pwd" value={passwd} onChange={(e) => {setPasswd(e.target.value)}}/>
+				</div>
+        
+				<div className="input_wrap">
+          <div className="category">
+            <input type="radio" name="cat"id="student" checked="checked" value={category} onChange={(e) => {setCategory(e.target.value)}}/>
+            <label for="student">Student</label>
+            <input type="radio" name="cat" id="educator" checked="checked" value={category} onChange={(e) => {setCategory(e.target.value)}}/>
+            <label for="educator">Educator</label>
+          </div>
+
+				</div>
+				
+				
+				<div className="input_wrap">
+					<input type="submit" value="Create Account" className="submit_btn"/>
+				</div>
+			</div>
+		</form>
+
+	</div>
+</div>
+
+
+
+    </div>
+  );
+}
+
+export default Sign_up;
