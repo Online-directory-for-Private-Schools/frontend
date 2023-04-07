05e71ad5021250396f00010edb49e20af0688a98
RayanZaki <rayan.hassici@ensia.edu.dz>
2023-04-07 15:02:19 +0100

    Added Hero Section

--- components/landing/Hero/Hero.tsx
+++ components/landing/Hero/Hero.tsx
@@ -1,9 +1,7 @@
 // @flow
 import * as React from "react";
 
-type Props = {};
-
-export function Hero(props: Props) {
+export function Hero() {
   return (
     <div className={"flex flex-col justify-around gap-10 py-52 text-dark-blue"}>
       <h1 className={"font-bold lg:text-8xl text-5xl text-center" + ""}>
