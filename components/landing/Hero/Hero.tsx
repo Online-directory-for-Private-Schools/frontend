92ae79fcee128248430d9ddc61f1f8cbb6329845
RayanZaki <rayan.hassici@ensia.edu.dz>
2023-04-07 15:01:53 +0100

    Added Hero Section

--- components/landing/Hero/Hero.tsx
+++ components/landing/Hero/Hero.tsx
@@ -4,5 +4,15 @@ import * as React from "react";
 type Props = {};
 
 export function Hero(props: Props) {
-  return <div></div>;
+  return (
+    <div className={"flex flex-col justify-around gap-10 py-52 text-dark-blue"}>
+      <h1 className={"font-bold lg:text-8xl text-5xl text-center" + ""}>
+        Find the best educators in Algeria
+      </h1>
+      <h2 className={"text-xl text-center"}>
+        Our first of a kind platform joins you with best institutes, <br />
+        schools and teachers in the country
+      </h2>
+    </div>
+  );
 }
