90eabbbe9ba6c517f6f61c0fe36ab7756726dba1
rayanzaki <rayan.hassici@ensia.edu.dz>
2023-05-06 20:26:56 +0100

    Added icons and props to CourseCard

--- components/Home/Courses.ts
+++ components/Home/Courses.ts
@@ -1,5 +1,3 @@
-import { SchoolCardProps } from "@/interfaces/SchoolCard";
-import CourseCard from "@/components/School/CourseCard";
 import { CourseCardProps } from "@/interfaces/CourseCardProps";
 
 const courses: Array<CourseCardProps> = [
