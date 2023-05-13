35e65e0fe1091b47c42c3b2b1580ee6a84d15a44
Ahmed Djalal Hacini <ahmed.hacini@ensia.edu.dz>
2023-05-13 19:16:19 +0100

    Added Profile and Tab

---
+++ pages/schoolProfile.tsx
@@ -0,0 +1,12 @@
+import { SchoolProfile } from "@/components/School/SchoolProfile";
+import React from "react";
+
+
+
+export default function schoolProfile(){
+
+    return (
+        <SchoolProfile/>
+    );
+    
+}
