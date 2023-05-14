24e3d0c4ae8c6a56f3f74efd6456b889cab81269
rayanzaki <rayan.hassici@ensia.edu.dz>
2023-05-14 14:08:45 +0100

    added put and delete

--- requestHandlers/REST-Handler/RequestHandler.ts
+++ requestHandlers/REST-Handler/RequestHandler.ts
@@ -25,7 +25,7 @@ export class RequestHandler {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer " + cookie.get("TOKEN"),
+            Authorization: "Bearer " + cookie.get("token"),
           },
         }
       ).catch((e) => console.log(e));
@@ -55,6 +55,65 @@ export class RequestHandler {
           message: e.message,
         },
       };
+    }
+  }
+
+  async delete(route: string, id: string) {
+    try {
+      let res = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_URL + route + "/" + id,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + cookie.get("token"),
+          },
+        }
+      ).catch((e) => console.log(e));
+
+      // @ts-ignore
+      if ("json" in res) {
+        res = await res.json();
+      }
+      // @ts-ignore
+      if (!!res.error)
+        // if an error occurred
+        return {
+          error: {
+            // @ts-ignore
+            message: res.error.message,
+          },
+        };
+      else {
+        return {
+          data: res,
+        };
+      }
+    } catch (e: any) {
+      // if an error occurred
+      return {
+        error: {
+          message: e.message,
+        },
+      };
+    }
+  }
+
+  async put(route: string, id: string, body: object) {
+    try {
+      return await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_URL + route + "/" + id,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + cookie.get("token"),
+          },
+          body: JSON.stringify(body),
+        }
+      ).then((res) => res.json());
+    } catch (e: any) {
+      return { error: { message: e.message } };
     }
   }
 }
