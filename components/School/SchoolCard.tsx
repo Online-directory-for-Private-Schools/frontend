08fc3f598578e2cf06bf1ceda3b167a3538d65f2
rayanzaki <rayan.hassici@ensia.edu.dz>
2023-05-27 16:57:56 +0100

    school card bg white

--- components/School/SchoolCard.tsx
+++ components/School/SchoolCard.tsx
@@ -61,7 +61,7 @@ const SchoolCard = ({
   return (
     <div
       className={
-        "z-40 flex flex-col gap-4 w-full bg-gray-300" +
+        "z-40 flex flex-col gap-4 w-full bg-white" +
         " h-44 rounded-xl border-2 p-3 shadow-[0_5px_10px] " +
         "shadow-gray-900/20 overflow-hidden " +
         "duration-300 hover:-translate-y-[5px] transform text-xl"
